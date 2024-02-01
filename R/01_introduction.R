6b15e969c192c1a0c987d967df1e470dc33b22da
EconMaett <matthias.spichiger@bluewin.ch>
2024-02-01 16:28:45 +0100

    Initial commit

---
+++ R/01_introduction.R
@@ -0,0 +1,50 @@
+# 01 - fastverse introduction
+
+# The `fastverse` R package is available on:
+# - CRAN: https://cran.r-project.org/package=fastverse
+# - GitHub: https://github.com/fastverse
+# - Website: https://fastverse.github.io/fastverse/
+# - R-universe: https://fastverse.r-universe.dev/builds
+
+## Setup ----
+
+# The R package is available on CRAN
+# install.packages("fastverse")
+library(fastverse)
+
+# attaching packages `data.table`, `magrittr`, `kit`, `collapse`
+
+
+# Vignette Introduciton to the fastverse
+# https://cran.r-project.org/web/packages/fastverse/vignettes/fastverse_intro.html
+
+# Check for updates
+fastverse_update()
+
+
+## Extending the *fastverse* for the session ----
+
+fastverse_extend(xts, roll, fasttime)
+
+fastverse_packages()
+
+options("fastverse.extend")
+
+
+## Permanent extensions ----
+
+
+## Custom *fastverse* configurations for projects ----
+
+
+## Creating separate package-verses ----
+
+
+## Dependencies, situational reports, and updating ----
+
+
+## Other *fastverse* options ----
+
+## Conclusion -----
+
+# END
---
+++ README.md
@@ -0,0 +1,12 @@
+# fastverse: A Suite of High-Performance Packages for Statistics and Data Manipulation
+
+The fastverse R package is available on [CRAN](https://cran.r-project.org/package=fastverse), the [R-universe](https://fastverse.r-universe.dev/builds),  [GitHub](https://github.com/fastverse), and [the package website](https://fastverse.github.io/fastverse/).
+
+Check out the vignette [Introduction to the fastverse](https://cran.r-project.org/web/packages/fastverse/vignettes/fastverse_intro.html).
+
+# Citation
+
+Krantz S (2023). _fastverse: A Suite of
+High-Performance Packages for Statistics and Data
+Manipulation_. R package version 0.3.2,
+<https://CRAN.R-project.org/package=fastverse>.
